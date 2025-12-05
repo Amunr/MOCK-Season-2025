8421207177afa22946bd13bbd701eaa9690aad55
Amunr <amunreddy@gmail.com>
2025-12-04 18:55:27 -0800

    modified:   src/main/java/frc/robot/RobotContainer.java 	modified:   src/main/java/frc/robot/commands/Autos.java 	modified:   src/main/java/frc/robot/commands/ExampleCommand.java 	modified:   src/main/java/frc/robot/subsystems/DriveSubsystem.java 	modified:   src/main/java/frc/robot/subsystems/Shooter.java

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,13 +4,47 @@
 
 package frc.robot.subsystems;
 
+import java.io.ObjectInputFilter.Config;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public DriveSubsystem() {}
-  
+
+  public static SparkMax leftOne = new SparkMax(1, MotorType.kBrushless);
+  public static SparkMax leftTwo = new SparkMax(2, MotorType.kBrushless);
+    public static  SparkMax rightOne = new SparkMax(3, MotorType.kBrushless);
+    public static SparkMax rightTwo = new SparkMax(4, MotorType.kBrushless);
+    public SparkMaxConfig left = new SparkMaxConfig();
+    public SparkMaxConfig right = new SparkMaxConfig();
+
+   public XboxController driveController = new XboxController(0);
+
+  public DriveSubsystem() {
+    left
+    .inverted(false)
+    .idleMode(IdleMode.kCoast);
+    
+    leftOne.configure(left, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    leftTwo.configure(left, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    right
+    .inverted(true)
+    .idleMode(IdleMode.kCoast); 
+
+    rightOne.configure(right, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightTwo.configure(right, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
 
   /**
    * Example command factory method.
@@ -38,7 +72,20 @@ public class DriveSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
+    leftOne.set(driveController.getLeftY());
+    leftTwo.set(driveController.getLeftY());
+    rightOne.set(driveController.getRightY());
+    rightTwo.set(driveController.getRightY());
 
+    if(driveController.getLeftY() <0.05 && driveController.getLeftY() >-0.05){
+      leftOne.stopMotor();
+      leftTwo.stopMotor();
+    }
+
+    if(driveController.getRightY() <0.05 && driveController.getRightY() >-0.05){
+      rightOne.stopMotor();
+      rightTwo.stopMotor();
+    }
   }
 
   @Override
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,22 +1,59 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-//
+
 package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.XboxController;
+import com.revrobotics.spark.SparkBase.ResetMode;
+
 
 public class Shooter extends SubsystemBase {
-
-  // Wr
+  public static XboxController operatorXboxController = new XboxController(1);
+   public static SparkMax shooterMotor = new SparkMax(5, MotorType.kBrushless);
+  // Write hthe shooter code. the ID of the shooter motor is 5. 
+  // Your task is to setup and confiugte the motor. And in the function shoot, have the motor have a speed set to 0.9. 
+  // in stop motor the motor should stop
+  // YOU are not setting up a controller, there is another method I will show next time we meet. 
   public Shooter() {
+    SparkMax max = new SparkMax(1, MotorType.kBrushless);
+SparkMaxConfig config = new SparkMaxConfig();
+
+config
+    
+    .idleMode(IdleMode.kCoast);
+shooterMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
   }
 
 
   public void shoot() {
+     shooterMotor.set(0.9);
+  }
+  public void stopMotor(){
+    shooterMotor.stopMotor();
+
   }
 
-
+  @Override
+  public void periodic(){
+   // USE right bumber
+      if(operatorXboxController.getRightBumper())
+      {
+        shoot(); 
+      }
+      if(operatorXboxController.getRightBumperReleased())
+      {
+       stopMotor();
+      }
+  }
 }
